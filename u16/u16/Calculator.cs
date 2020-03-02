69b4650f54f402a9f5d873e4f67d0c1d88bef69e
saeed-c <55282225+saeed-c@users.noreply.github.com>
2020-03-02 15:30:12 +0000

    Add files via upload

---
+++ u16/u16/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U16_Modular_Calculator
+{
+    class Calculator
+    {
+        //Rectangle Area function to calculate the aree of a rectangle using the formula
+        public double Rectangle(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        //Circle Area Function to calculate the area of a circle using the formula
+        public double Circle(double radius)
+        {
+
+
+            double pi = Math.PI;
+
+
+            return pi * (radius * radius); ;
+
+        }
+
+        //Triangle area function to calculate the area of the triangle using the formula
+        public double Triangle(double num1, double num2)
+        {
+            return num1 * num2 / 2;
+        }
+
+
+
+
+    }
+}
